a4902e364a9cfcddac78efc2c75b9f295e2876ce
Manon <manon.chp68@gmail.com>
2025-03-27 14:06:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Ajout produit</title>
+</head>
+<body>
+    <form action="traitement.php" method="post">
+        <p>
+            <label>
+                Nom du produit:
+                <input type="text" name="name">
+            </label>
+        </p>
+            <label>
+                Prix du produit:
+                <input type="number" name="price" step="0.1">
+            </label>
+        </p>
+            <label>
+                Quantité désirée: 
+                <input type="number" name="qtt" value="1">
+            </label>
+        </p>
+            <input type="submit" name="submit" value="Ajouter le produit">
+        </p>
+    </form> 
+</body>
+</html>
+
