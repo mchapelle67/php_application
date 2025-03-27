3004e9493600e1004967e57deeaf80a4176e0d83
Manon <manon.chp68@gmail.com>
2025-03-27 18:56:43 +0100

    Ajout PHP, HTML et CSS

--- index.php
+++ index.php
@@ -3,30 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="styles.css"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    
     <title>Ajout produit</title>
 </head>
 <body>
-    <form action="traitement.php" method="post">
-        <p>
-            <label>
-                Nom du produit:
-                <input type="text" name="name">
-            </label>
-        </p>
-            <label>
-                Prix du produit:
-                <input type="number" name="price" step="0.1">
-            </label>
-        </p>
-            <label>
-                Quantité désirée: 
-                <input type="number" name="qtt" value="1">
-            </label>
-        </p>
-            <input type="submit" name="submit" value="Ajouter le produit">
-        </p>
-    </form> 
+    <header>
+        <img class="logo" src="logo.png" alt="Logo">
+        <nav class="navigateur">
+            <a href="index.php">Formulaire de commande</a>
+            <a href="recap.php">Tableaux de commandes</a>
+        </nav>
+    </header>
+   <main>
+        <form action="traitement.php" method="post">
+            <h1> Formulaire de commande </h1>
+                <p>
+                    <label> Nom du produit:</label>
+                        <input type="text" name="name" placeholder="Ex: Banane">
+                </p>
+                <p>
+                    <label>Prix du produit:</label>
+                        <input type="number" name="price" step="0.1" placeholder="€">
+                </p>
+                <p>
+                    <label>Quantité désirée:</label> 
+                        <input type="number" name="qtt" value="1">
+                </p>
+                <p class="button">
+                        <input type="submit" name="submit" value="Ajouter le produit">
+                </p>
+        </form>
+    </main>
+    <footer>
+    </footer>
 </body>
 </html>
 
--- recap.php
+++ recap.php
@@ -6,44 +6,57 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css"> 
+
 
     <title>Récapitulatif des produits </title>
 </head>
 <body>
-    <?php 
-        if(!isset($_SESSION["products"]) || empty($_SESSION["products"])){
-        echo "<p>Aucun produit en session...</p>";
-        } else {
-            echo "<table>",
-                    "<thead>",
-                        "<tr>",
-                            "<th>#</th>",
-                            "<th>Nom</th>",
-                            "<th>Prix</th>",
-                            "<th>Quantité</th>",
-                            "<th>Total</th>",
-                        "</tr>",
-                    "</thead>",
-                "<tbody>";
-                    $totalGeneral = 0;
-                    foreach($_SESSION["products"] as $index =>$product){
-                        echo "<tr>",
-                                "<td>".$index."</td>",
-                                "<td>".$product["name"]."</td>",
-                                "<td>".number_format($product["price"], 2, ",", "&nbsp")."&nbsp;€</td>",
-                                "<td>".$product["qtt"]."</td>",
-                                "<td>".number_format($product["total"], 2, ",", "&nbsp")."&nbsp;€</td>",
-                            "</tr>";
-                        $totalGeneral += $product["total"];
-                    }
+<header>
+        <img class="logo" src="logo.png" alt="Logo">
+        <nav class="navigateur">
+            <a href="index.php">Formulaire de commande</a>
+            <a href="recap.php">Tableaux de commandes</a>
+        </nav>
+    </header>
+    <main>
+        <?php 
+            if(!isset($_SESSION["products"]) || empty($_SESSION["products"])){
+            echo "<p>Aucun produit en session...</p>";
+            } else {
+                echo "<table class='table-container'>",
+                        "<thead>",
+                            "<tr>",
+                                "<th>#</th>",
+                                "<th>Nom</th>",
+                                "<th>Prix</th>",
+                                "<th>Quantité</th>",
+                                "<th>Total</th>",
+                            "</tr>",
+                        "</thead>",
+                    "<tbody>";
+                $totalGeneral = 0;
+                foreach($_SESSION["products"] as $index =>$product){
                     echo "<tr>",
-                            "<td colspan=4>Total général: </td>",
+                            "<td>".$index."</td>",
+                            "<td>".$product["name"]."</td>",
+                            "<td>".number_format($product["price"], 2, ",", "&nbsp")."&nbsp;€</td>",
+                            "<td>".$product["qtt"]."</td>",
+                            "<td>".number_format($product["total"], 2, ",", "&nbsp")."&nbsp;€</td>",
+                        "</tr>";
+                    $totalGeneral += $product["total"];
+                }
+                    echo "<tr>",
+                            "<td class='tgeneral' colspan=4>Total général: </td>",
                             "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp")."&nbsp;€</td>" ,
                         "</tr>",
                     "</tbody>",
                     "</table>";
         }
     ?>
+    </main>
+    <footer>
+    </footer>
 </body>
 </html>
 
